2bfab352f0242c054ef2040fee1dc55dbc6888f1
nomanajaz65 <nomanajaz65@gmail.com>
2023-04-15 04:47:33 -0700

    done

---
+++ app.js
@@ -0,0 +1,11 @@
+function bulbon(){
+    document.getElementById('abc').src='./bulbon.jpg'
+}
+
+function bulboff(){
+    document.getElementById('abc').src="./bulboff.jpg"
+}
+
+function none(){
+    document.getElementById('abc').src="none"
+}
